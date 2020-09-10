11ffa41fdaf94b22df741c990749402c44fc7f1a
eloquent_rose <keerthanaravikumarnaidu@gmail.com>
2020-09-10 19:05:02 +0530

    Done with GUI

--- flames.py
+++ flames.py
@@ -1,4 +1,6 @@
-from tkinter import *
+import tkinter as tk
+from tkinter import messagebox as mb
+from functools import partial  
 # ---------------------------------------------------- FLames Implementation -------------------------------------------------------------
 
 name_1 = []
@@ -6,166 +8,182 @@ name_2 = []
 count = 0
 flag = True
 
+# -------------------------------------------------------- Initialization ---------------------------------------------------------------
 
-print("# --------------------- FLames Implementation ---------------------")
-while(flag):
-    name1 = input ( "Enter the boy's name : " )
+def Editing():
+
+    # mb.showerror("Error", "Hello")
+    name1 = str(entry1.get())
     name1 = name1.lower()
     name1 = name1.replace(" ", "")
-
-    while(name1.isalpha() == False):
-        print("The Entered name contains invalid characters. \nEnter the string again")
-        name1 = input ( "Enter the boy's name : " )
-        name1 = name1.lower()
-        name1 = name1.replace(" ", "")
-
-
-    name2 = input ( "Enter the girl's name : " )
+    
+    name2 = str(entry2.get())
     name2 = name2.lower()
     name2 = name2.replace(" ", "")
 
-    while(name2.isalpha() == False):
-        print("The Entered name contains invalid characters. \nEnter the string again")
-        name2 = input ( "Enter the boy's name : " )
-        name2 = name2.lower()
-        name2 = name2.replace(" ", "")
+    if(name1.isalpha() == False or name2.isalpha() == False):
+        Str = "The Entered name contains invalid characters. \nEnter the string again"
+        # Result.config(text = "%s" %Str) 
+        mb.showerror("Error",Str)
+        return 
 
-    if name1 != name2:
-        flag = False
-    else:
-        print("Same names have been entered")
-
-
-
-class Flames:
-
-# -------------------------------------------------------- Initialization ---------------------------------------------------------------
-
-    def __init__(self, name1 = name1, name2 = name2):
-        self.name1 = name1
-        self.name2 = name2
+    if name1 == name2:
+        Str = "Same names have been entered"
+        # Result.config(text = "%s" %Str)
+        mb.showerror("Error",Str) 
+        return 
+    
+    cancel_comman_letters(name1, name2)
+    letter_count()
+    flames()
+    
 
 # ----------------------------------------- Cancellation of Comman letters between 2 names -----------------------------------------------
 
-    def cancel_comman_letters(self):
+def cancel_comman_letters(name1, name2):
 
-        global name_1 
-        global name_2 
+    global name_1 
+    global name_2 
 
-        name_1 = []
-        name_2 = []
+    name_1 = []
+    name_2 = []
         
-        for i in self.name1 :
-            name_1.append(i)
+    for i in name1 :
+        name_1.append(i)
 
-        for j in self.name2 :
-            name_2.append(j)
+    for j in name2 :
+        name_2.append(j)
 
-        for i in range (len(name_1)):
+    for i in range (len(name_1)):
 
-            for j in range (len(name_2)):
+        for j in range (len(name_2)):
 
-                if name_1[i] == name_2[j]:
-                    name_1[i] = '$'
-                    name_2[j] = '$'
+            if name_1[i] == name_2[j]:
+                name_1[i] = '$'
+                name_2[j] = '$'
 
         # print (name_1)
         # print (name_2)
+        # return name_1, name_2
 
 # ------------------------------------------- Count the remaining letters left in the names ----------------------------------------------
 
-    def letter_count(self):
+def letter_count():
 
-        global count
-        count = 0
+    global count
+    count = 0
 
-        for i in range (len(name_1)):
-            if name_1[i] != '$':
-                count = count + 1
+    for i in range (len(name_1)):
+        if name_1[i] != '$':
+            count = count + 1
             
-        for j in range (len(name_2)):
-            if name_2[j] != '$':
-                count = count + 1
+    for j in range (len(name_2)):
+        if name_2[j] != '$':
+            count = count + 1
 
-        # print (count)
+    # print (count)
 
 # ---------------------------------------------------- Finding the answer in FLAMES ------------------------------------------------------
 
-    def flames(self):
-        print("Performing Flames...")
-        global count
-        nc = count
-        flames = ['f', 'l', 'a', 'm', 'e', 's']
-        alter = []
-        i = 0
-        
-        remain = 0
-        flag_check = True
+def flames():
+    print("Performing Flames...")
+    global count
+    nc = count
+    flames = ['f', 'l', 'a', 'm', 'e', 's']
+    alter = []
+    i = 0
+    
+    flag_check = True
 
-        while flag_check == True:
-            if i != count:
-                i = i + 1
+    while flag_check == True:
+        if i != count:
+            i = i + 1
 
-            if i == count:
-                flames[i-1] = '*'
-                i = i - 1
+        if i == count:
+            flames[i-1] = '*'
+            i = i - 1
 
-                for j in range (i, len(flames)):
-                    if flames[j] != '*':
-                        alter.append(flames[j])
-                for j in range (0, i):
-                    if flames[j] != '*':
-                        alter.append(flames[j])
+            for j in range (i, len(flames)):
+                if flames[j] != '*':
+                    alter.append(flames[j])
+            for j in range (0, i):
+                if flames[j] != '*':
+                    alter.append(flames[j])
 
-                flames = []
-                for k in range (len(alter)):
-                    flames.append(alter[k])
-                alter = []
-                i = 0
+            flames = []
+            for k in range (len(alter)):
+                flames.append(alter[k])
+            alter = []
+            i = 0
 
-                count = nc
-            if i == len(flames):
-                p = i
-                count = count - p
-                i = 0
+            count = nc
+        if i == len(flames):
+            p = i
+            count = count - p
+            i = 0
 
-            if len(flames) == 1:
-                if flames[0] == 'f':
-                    print("They are FRIENDs")
-                if flames[0] == 'l':
-                    print("They are LOVEd by each other")
-                if flames[0] == 'a':
-                    print("One has AFFECTION towards the other")
-                if flames[0] == 'm':
-                    print("They will marry each other")
-                if flames[0] == 'e':
-                    print("They are ENEMIES")
-                if flames[0] == 's':
-                    print("SISTER!!!!")
-                
-                flag_check = False
+        if len(flames) == 1:
+            if flames[0] == 'f':
+                # print("They are FRIENDs")
+                mb.showinfo("Result", "They are FRIENDs")
+            if flames[0] == 'l':
+                # print("They are LOVEd by each other")
+                mb.showinfo("Result", "They are LOVEd by each other")
+            if flames[0] == 'a':
+                # print("One has AFFECTION towards the other")
+                mb.showinfo("Result", "One has AFFECTION towards the other")
+            if flames[0] == 'm':
+                mb.showinfo("Result","They will marry each other")
+            if flames[0] == 'e':
+                mb.showinfo("Result","They are ENEMIES")
+            if flames[0] == 's':
+                mb.showinfo("Result","SISTER!!!!")
+            
+            flag_check = False
                     
 
 
 # --------------------------------------------------------- Function Calls --------------------------------------------------------------
 
-X = Flames()
-X.cancel_comman_letters()
-X.letter_count()
-X.flames()
+
+# cancel_comman_letters(name1, name2)
+# letter_count(name1, name2)
+# flames(name1, name2)
 
 # ---------------------------------------------------------------- GUI -------------------------------------------------------------------
 
-GUI = Tk()
-GUI.geometry ( "500x500" )
+GUI = tk.Tk()
+GUI.geometry ( "450x350" )
+
+winWidth = GUI.winfo_reqwidth()
+winHeight = GUI.winfo_reqheight()
+# print(winWidth,"     " ,winHeight)
+
+posRight = int(GUI.winfo_screenwidth()/2 - winWidth)
+posLeft = int(GUI.winfo_screenheight()/2 - winHeight)
+# print(posRight," ",posLeft)
+
+GUI.geometry( "+{}+{}" .format(posRight, posLeft))
+
 GUI.resizable ( 0, 0 )
 GUI.title ( "FLAMES" )
-Label ( text = "Boy's Name : " ).grid(row = 0)
-Label ( text = "Girl's Name : " ).grid(row = 1)
 
-entry1 = Entry(GUI)
-entry2 = Entry(GUI)
+BN = tk.StringVar()
+GN = tk.StringVar()
 
-entry1.grid(row = 0, column = 1)
-entry2.grid(row = 1, column = 1)
+tk.Label( GUI, text = "Boy's Name : " ).place( x = 120, y = 50 )  
+tk.Label( GUI, text= "Girl's Name : " ).place( x = 120, y = 80 )
+
+entry1 = tk.Entry(GUI, textvariable = BN)
+entry1.place( x = 210, y = 50 )
+
+entry2 = tk.Entry(GUI, textvariable = GN)
+entry2.place( x = 210, y = 80 )
+
+Result = tk.Label(GUI)
+Result.grid( row = 5, column = 5)
+
+tk.Button(GUI, text = "Submit",activebackground = "pink", activeforeground = "blue", command = Editing).place(x = 200, y = 120)  
+
+
 GUI.mainloop()
